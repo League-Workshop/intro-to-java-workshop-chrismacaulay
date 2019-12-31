ac8ce1b4167f276faf6ab767ad0c5d8d2ac6e22b
League Student <league@iMac-13.attlocal.net>
2019-12-31 14:56:27 -0800

    Made Frog Boi Do Stuff

--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,22 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot Bill = new Robot();
+	Bill.changeRobot("https://i.kym-cdn.com/photos/images/original/001/134/408/565.png");	
 		// 5. Set your robot's pen down 
-		
+		Bill.penDown();
 		// 3. Set the robot to go at max speed (100)
-		
+		Bill.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
+		for(int i = 0;i<1000000; i++) {
 			// 7. Change the pen color to random
-		
+		Bill.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
-			
+		Bill.move(101/100*i);
 			// 2. Turn the robot 360/7 degrees to the right
-		
+		Bill.turn(360/7);
 			// 8. Set the pen width to i
-			
-	}
+		Bill.setPenWidth(i);	
+	}	
+}
 }
--- src/section2/TriangleShell.java
+++ src/section2/TriangleShell.java
@@ -5,30 +5,40 @@ import org.jointheleague.graphical.robot.Robot;
 public class TriangleShell {
 
 	// 1. Create a new Robot
-
+Robot Joe = new Robot();
 	
 	void go() {
-		drawTriangle(100); // 3. delete this line (used only for testing)
+		
+		Joe.changeRobot("https://tr.rbxcdn.com/39a0f17caecc92d3addf77ec05227921/420/420/Hat/Png");
 
+		Joe.penDown();
 		// 6. Make the robot go as fast as possible
-
+		Joe.setSpeed(100);
 		// 4. make a variable to hold the length of the triangle and set it to 50
-
+		int length = 50;
 		// 7. Use a for loop to repeat steps #9 to #10, 60 times
-
+for(int i = 0; i<60;i++) {
 			// 9. Change the color of the pen to a random color
-	
+	Joe.setPenWidth(25);
+	Joe.setRandomPenColor();
 			// 8. Increase the length variable by 10
-	
+	 length = length + 50;
 			// 5. call your drawTriangle() method using your length variable
-	
+	drawTriangle(length);
 			// 10. Turn the robot 6 degrees to the right
-
+		Joe.turn(6);
+		}	
 	}
 
 	/* 2. fill in the method below to draw a triangle. Use the length variable when you call move(). */
 	private void drawTriangle(int length) {
 		
+		Joe.turn(120);
+		Joe.move(length);
+		Joe.turn(120);
+		Joe.move(length);
+		Joe.turn(120);
+		Joe.move(length);
 		
 		
 	}
